5bc4f61658bf6f4b4e34a37c451474a17a5f45e5
Hunter MacDermut <whosebluesanyway@gmail.com>
2015-10-13 17:20:34 -0600

    Begin wiring-up game loop

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
-var game = new Phaser.Game(300, 300, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+var game = new Phaser.Game(300, 325, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 var tileArray = [];
+var spillMeter = 0;
+var mopMeter = 0;
+var score = 0;
+var spillText, mopText, scoreText;
 
 function preload(){
     game.stage.backgroundColor = '#fff';
@@ -15,7 +19,8 @@ function create(){
         this.sprite = game.add.sprite(this.x, this.y, 'beaker');
         this.sprite.frame = 0;
         this.delay = 1;
-        this.text = game.add.text(this.x + 5, this.y + 5, this.delay, {font: '18px Helvetica', fill: 'black'});
+        this.maxDelay = 1;
+        this.text = game.add.text(this.x + 5, this.y + 5, this.delay + '/' + this.maxDelay, {font: '18px Helvetica', fill: 'black'});
         this.sprite.inputEnabled = true;
         this.sprite.events.onInputDown.add(this.dilute, this);
         tileArray.push(this);
@@ -23,13 +28,16 @@ function create(){
 
     Tile.prototype.dilute = function(){
         this.delay += 1;
+        this.maxDelay += 1;
+
+        game.time.events.add(500, countdown, this).autoDestroy = true;
     }
 
     Tile.prototype.change = function(){
         if (this.sprite.frame === 0){
             this.sprite.frame = 1;
-            this.delay = 1;
-            this.text.setText(this.delay);
+            this.delay = this.maxDelay;
+            this.text.setText(this.delay + '/' + this.maxDelay);
         } else {
             this.sprite.frame = 2;
         }
@@ -72,20 +80,26 @@ function create(){
     tile54 = new Tile(250, 200, [5, 4]);
     tile55 = new Tile(250, 250, [5, 5]);
 
-    // testing
-    tile00.sprite.frame = 1;
-    tile01.sprite.frame = 2;
-    tile02.sprite.frame = 2;
-    tile20.sprite.frame = 2;
-    tile30.sprite.frame = 2;
-    tile50.sprite.frame = 2;
-    tile51.sprite.frame = 2;
+    fontStyle = {font: '18px Helvetica', fill: 'black'};
+    scoreText = game.add.text(5, 305, 'Score: ' + score, fontStyle);
+    mopText = game.add.text(110, 305, 'Mop: ' + mopMeter, fontStyle);
+    spillText = game.add.text(210, 305, 'Spill: ' + spillMeter, fontStyle);
+
+    // randomize game start
+    for (var i = 0; i < tileArray.length; i++) {
+        if (Math.round(Math.random()) === 0){
+            tileArray[i].sprite.frame = 1;
+        }
+    };
 }
 
 function update(){
     for (var i = 0; i < tileArray.length; i++) {
-        tileArray[i].text.setText(tileArray[i].delay);
+        tileArray[i].text.setText(tileArray[i].delay + '/' + tileArray[i].maxDelay);
     };
+    scoreText.setText('Score: ' + score);
+    mopText.setText('Mop: ' + mopMeter);
+    spillText.setText('Spill: ' + spillMeter);
 }
 
 function countdown(){
@@ -93,15 +107,22 @@ function countdown(){
     // remaining delay by 1
     for (var i = 0; i < tileArray.length; i++) {
         tileArray[i].delay -= 1;
+        tileArray[i].text.setText(tileArray[i].delay);
     };
+
+    game.time.events.add(500, changeColor, this).autoDestroy = true;
 }
 
 function changeColor(){
     // loop through all beakers and change
     // color of those at 0 delay
     for (var i = 0; i < tileArray.length; i++) {
-        tileArray[i].change();
+        if (tileArray[i].delay === 0){
+            tileArray[i].change();
+        }
     };
+
+    game.time.events.add(500, filterOutBlack, this).autoDestroy = true;
 }
 
 function filterOutBlack(){
@@ -113,6 +134,9 @@ function filterOutBlack(){
             filterOutBlackArray.push(tileArray[i]);
         }
     };
+
+    game.time.events.add(500, hasNeighbors, this, filterOutBlackArray).autoDestroy = true;
+
     return filterOutBlackArray;
 }
 
@@ -120,11 +144,15 @@ function hasNeighbors(blackBeakers){
     // loop through black beakers and return array
     // of beakers where their N, E, S, W neighbors also black
     var hasNeighborsArray = [];
+    var isSolitaryArray = [];
     for (var i = 0; i < blackBeakers.length; i++) {
         if (tileFromCoords([blackBeakers[i].coords[0], blackBeakers[i].coords[1] - 1]) !== undefined){
             // if N tile exists AND beaker is black
             if (tileFromCoords([blackBeakers[i].coords[0], blackBeakers[i].coords[1] - 1]).sprite.frame === 2){
                 hasNeighborsArray.push(blackBeakers[i]);
+                continue;
+            } else {
+                isSolitaryArray.push(blackBeakers[i]);
                 continue;
             }
         }
@@ -133,12 +161,18 @@ function hasNeighbors(blackBeakers){
             if (tileFromCoords([blackBeakers[i].coords[0] + 1, blackBeakers[i].coords[1]]).sprite.frame === 2){
                 hasNeighborsArray.push(blackBeakers[i]);
                 continue;
+            } else {
+                isSolitaryArray.push(blackBeakers[i]);
+                continue;
             }
         }
         if (tileFromCoords([blackBeakers[i].coords[0], blackBeakers[i].coords[1] + 1]) !== undefined){
             // if S tile exists AND beaker is black
             if (tileFromCoords([blackBeakers[i].coords[0], blackBeakers[i].coords[1] + 1]).sprite.frame === 2){
                 hasNeighborsArray.push(blackBeakers[i]);
+                continue;
+            } else {
+                isSolitaryArray.push(blackBeakers[i]);
                 continue;
             }
         }
@@ -147,19 +181,51 @@ function hasNeighbors(blackBeakers){
             if (tileFromCoords([blackBeakers[i].coords[0] - 1, blackBeakers[i].coords[1]]).sprite.frame === 2){
                 hasNeighborsArray.push(blackBeakers[i]);
                 continue;
+            } else {
+                isSolitaryArray.push(blackBeakers[i]);
+                continue;
             }
         }
     };
+    addToSpill(isSolitaryArray);
+    addToMop(hasNeighborsArray);
+    // game.time.events.add(500, resetBeakers, this, blackBeakers).autoDestroy = true;
     return hasNeighborsArray;
 }
 
-function resetBeakers(blackBeakersWithNeighbors){
+function debugFunc(array){
+    for (var i = 0; i < array.length; i++) {
+        console.log(array[i].coords);
+    };
+    console.log('=-=-=-=-=-=-=-=');
+}
+
+function addToSpill(solitaryBlackBeakers){
+    // add 1 to spill meter for every
+    // solitary black beaker
+    console.log(solitaryBlackBeakers);
+    for (var i = 0; i < solitaryBlackBeakers.length; i++) {
+        spillMeter += 1;
+    };
+}
+
+function addToMop(blackBeakersWithNeighbors){
+    // add 1 to mop meter for every
+    // black beaker with a neighboring black beaker
+    for (var i = 0; i < blackBeakersWithNeighbors.length; i++) {
+        mopMeter += 1;
+        score += 1;
+    };
+}
+
+function resetBeakers(blackBeakers){
     // loop through all beakers marked for removal
     // and turn them clear, reset their delay to 1
-    for (var i = 0; i < blackBeakersWithNeighbors.length; i++) {
-        blackBeakersWithNeighbors[i].sprite.frame = 0;
-        blackBeakersWithNeighbors[i].delay = 1;
-        blackBeakersWithNeighbors[i].text.setText(blackBeakersWithNeighbors[i].delay);
+    for (var i = 0; i < blackBeakers.length; i++) {
+        blackBeakers[i].sprite.frame = 0;
+        blackBeakers[i].delay = 1;
+        blackBeakers[i].maxDelay = 1;
+        blackBeakers[i].text.setText(blackBeakers[i].delay);
     };
 }
 
@@ -186,6 +252,20 @@ things that need to happen every turn:
         If beaker +/-1x/y is black, add initial beaker to list of removable beakers
         Remove the beakers
 • all beakers flagged for removal are turned clear and their delays are reset --> resetBeakers()
+
+Loop:
+
+1. Player clicks/taps a beaker
+2. Delay on that beaker ticks up by 1 (max delay +1)
+3. Delay on ALL beakers ticks down by 1
+4. All clear beakers at 0 delay are changed to orange and delay is set to max delay (from clear stage)
+5. All orange beakers at 0 delay are changed to black.
+6. All black beakers with neighboring black beakers are totaled and added to score and mop meter(?)
+    6a. If mop meter reaches 36, reduce spill meter by 1, reset mop meter to 0
+7. All solitary black beakers are totaled and added to spill meter
+    7a. If spill meter reaches 6, game over
+8. All black beakers are reset to clear and delay to 1 (max delay to 1 also)
+9. Player takes another turn
 
 notes:
 
